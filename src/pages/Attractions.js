d55a58617028d0f84c625c39a65785a0562489fb
Pedro Miotti <pedro.miotti@6place.com.br>
2022-11-17 09:17:08 -0300

    chore: added github pages setup

--- src/pages/Attractions.js
+++ src/pages/Attractions.js
@@ -1,265 +1,238 @@
-import { Helmet } from 'react-helmet-async';
-import { filter } from 'lodash';
-import { sentenceCase } from 'change-case';
+import {Helmet} from 'react-helmet-async';
+import {filter} from 'lodash';
+import {sentenceCase} from 'change-case';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
 
 // @mui
 import {
-  Card,
-  Table,
-  Stack,
-  Paper,
-  Avatar,
-  Button,
-  Popover,
-  Checkbox,
-  TableRow,
-  MenuItem,
-  TableBody,
-  TableCell,
-  Container,
-  Typography,
-  IconButton,
-  TableContainer,
-  TablePagination,
+    Card,
+    Table,
+    Stack,
+    Paper,
+    Avatar,
+    Button,
+    Popover,
+    Checkbox,
+    TableRow,
+    MenuItem,
+    TableBody,
+    TableCell,
+    Container,
+    Typography,
+    IconButton,
+    TableContainer,
+    TablePagination, Box, CircularProgress,
 } from '@mui/material';
 // components
 import Label from '../components/label';
 import Iconify from '../components/iconify';
 import Scrollbar from '../components/scrollbar';
 // sections
-import { UserListHead, UserListToolbar } from '../sections/@dashboard/user';
-// mock
-import USERLIST from '../_mock/user';
+import {UserListHead, UserListToolbar} from '../sections/@dashboard/user';
+
 
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: 'name', label: 'Nome', alignRight: false },
-  { id: 'email', label: 'Email', alignRight: false },
-  { id: 'cellphone', label: 'Telefone', alignRight: false },
-  // { id: 'status', label: 'Status', alignRight: false },
-  // { id: '' },
+    {id: 'name', label: 'Nome', alignRight: false},
+    {id: 'email', label: 'Email', alignRight: false},
+    {id: 'cellphone', label: 'Telefone', alignRight: false},
 ];
 
 // ----------------------------------------------------------------------
 
 function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
 }
 
 function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
 function applySortFilter(array, comparator, query) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  if (query) {
-    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-  }
-  return stabilizedThis.map((el) => el[0]);
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    if (query) {
+        return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    }
+    return stabilizedThis.map((el) => el[0]);
 }
 
 export default function UserPage() {
-  const [open, setOpen] = useState(null);
+    const [open, setOpen] = useState(null);
+    const [page, setPage] = useState(0);
+    const [order, setOrder] = useState('asc');
+    const [selected, setSelected] = useState([]);
+    const [orderBy, setOrderBy] = useState('name');
+    const [filterName, setFilterName] = useState('');
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [users, setUsers] = useState([]);
+    const [isUsersRequestOnProgress, setIsUsersRequestOnProgress] = useState(false);
 
-  const [page, setPage] = useState(0);
+    useEffect(() => {
+        setIsUsersRequestOnProgress(true);
+        axios.get('https://api-qg-prime.azurewebsites.net/api/api-fetch-all-users').then(response => {
+            setUsers(response.data)
+            setIsUsersRequestOnProgress(false);
+        });
+    }, [])
 
-  const [order, setOrder] = useState('asc');
+    const handleCloseMenu = () => {
+        setOpen(null);
+    };
 
-  const [selected, setSelected] = useState([]);
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
 
-  const [orderBy, setOrderBy] = useState('name');
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = users.map((n) => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
 
-  const [filterName, setFilterName] = useState('');
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
 
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+    const handleChangeRowsPerPage = (event) => {
+        setPage(0);
+        setRowsPerPage(parseInt(event.target.value, 10));
+    };
 
-  const [ users, setUsers] = useState([]);
+    const handleFilterByName = (event) => {
+        setPage(0);
+        setFilterName(event.target.value);
+    };
 
-  useEffect(() => {
-    axios.get('https://api-qg-prime.azurewebsites.net/api/api-fetch-all-users').then(response => setUsers(response.data));
-  }, [])
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - users.length) : 0;
 
-  const handleCloseMenu = () => {
-    setOpen(null);
-  };
+    const filteredUsers = applySortFilter(users, getComparator(order, orderBy), filterName);
 
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
+    const isNotFound = !filteredUsers.length && !!filterName;
 
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = users.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
+    return (
+        <>
+            <Helmet>
+                <title> Atrações </title>
+            </Helmet>
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+            <Container>
+                <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+                    <Typography variant="h4" gutterBottom>
+                        Usuários
+                    </Typography>
+                </Stack>
 
-  const handleChangeRowsPerPage = (event) => {
-    setPage(0);
-    setRowsPerPage(parseInt(event.target.value, 10));
-  };
+                <Card>
+                    <UserListToolbar numSelected={selected.length} filterName={filterName}
+                                     onFilterName={handleFilterByName}/>
 
-  const handleFilterByName = (event) => {
-    setPage(0);
-    setFilterName(event.target.value);
-  };
+                    <Scrollbar>
+                        <TableContainer sx={{minWidth: 800}}>
+                            {isUsersRequestOnProgress ? (
+                                <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                                    <CircularProgress/>
+                                </Box>) : (
+                                <Table>
+                                    <UserListHead
+                                        order={order}
+                                        orderBy={orderBy}
+                                        headLabel={TABLE_HEAD}
+                                        rowCount={users.length}
+                                        numSelected={selected.length}
+                                        onRequestSort={handleRequestSort}
+                                    />
+                                    <TableBody>
+                                        {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                                            const {id, name, email, lastname, cellphone} = row;
+                                            const selectedUser = selected.indexOf(name) !== -1;
 
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - users.length) : 0;
+                                            const fullName = `${name} ${lastname}`;
+                                            return (
+                                                <TableRow hover key={id} tabIndex={-1} role="checkbox"
+                                                          selected={selectedUser}>
+                                                    <TableCell component="th" scope="row" padding="none">
+                                                        <Stack direction="row" alignItems="center" spacing={2}>
+                                                            <Avatar alt={name} sx={{ml: '10px'}}/>
+                                                            <Typography variant="subtitle2" noWrap>
+                                                                {fullName}
+                                                            </Typography>
+                                                        </Stack>
+                                                    </TableCell>
 
-  const filteredUsers = applySortFilter(users, getComparator(order, orderBy), filterName);
+                                                    <TableCell align="left">{email}</TableCell>
 
-  const isNotFound = !filteredUsers.length && !!filterName;
+                                                    <TableCell align="left">{cellphone}</TableCell>
 
-  return (
-    <>
-      <Helmet>
-        <title> Atrações </title>
-      </Helmet>
+                                                </TableRow>
+                                            );
+                                        })}
+                                        {emptyRows > 0 && (
+                                            <TableRow style={{height: 53 * emptyRows}}>
+                                                <TableCell colSpan={6}/>
+                                            </TableRow>
+                                        )}
+                                    </TableBody>
 
-      <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
-          Usuários
-          </Typography>
-        </Stack>
+                                    {isNotFound && (
+                                        <TableBody>
+                                            <TableRow>
+                                                <TableCell align="center" colSpan={6} sx={{py: 3}}>
+                                                    <Paper
+                                                        sx={{
+                                                            textAlign: 'center',
+                                                        }}
+                                                    >
+                                                        <Typography variant="h6" paragraph>
+                                                            Not found
+                                                        </Typography>
 
-        <Card>
-          <UserListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
+                                                        <Typography variant="body2">
+                                                            No results found for &nbsp;
+                                                            <strong>&quot;{filterName}&quot;</strong>.
+                                                            <br/> Try checking for typos or using complete words.
+                                                        </Typography>
+                                                    </Paper>
+                                                </TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                    )}
+                                </Table>
+                            )}
+                        </TableContainer>
+                    </Scrollbar>
 
-          <Scrollbar>
-            <TableContainer sx={{ minWidth: 800 }}>
-              <Table>
-                <UserListHead
-                  order={order}
-                  orderBy={orderBy}
-                  headLabel={TABLE_HEAD}
-                  rowCount={users.length}
-                  numSelected={selected.length}
-                  onRequestSort={handleRequestSort}
-                />
-                <TableBody>
-                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                    const { id, name, email, lastname, cellphone } = row;
-                    const selectedUser = selected.indexOf(name) !== -1;
-
-                    const fullName = `${name} ${lastname}`;
-                    return (
-                      <TableRow hover key={id} tabIndex={-1} role="checkbox" selected={selectedUser}>
-                        <TableCell component="th" scope="row" padding="none">
-                          <Stack direction="row" alignItems="center" spacing={2}>
-                            <Avatar alt={name} sx={{ml: '10px'}} />
-                            <Typography variant="subtitle2" noWrap>
-                              {fullName}
-                            </Typography>
-                          </Stack>
-                        </TableCell>
-
-                        <TableCell align="left">{email}</TableCell>
-
-                        <TableCell align="left">{cellphone}</TableCell>
-
-                      </TableRow>
-                    );
-                  })}
-                  {emptyRows > 0 && (
-                    <TableRow style={{ height: 53 * emptyRows }}>
-                      <TableCell colSpan={6} />
-                    </TableRow>
-                  )}
-                </TableBody>
-
-                {isNotFound && (
-                  <TableBody>
-                    <TableRow>
-                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                        <Paper
-                          sx={{
-                            textAlign: 'center',
-                          }}
-                        >
-                          <Typography variant="h6" paragraph>
-                            Not found
-                          </Typography>
-
-                          <Typography variant="body2">
-                            No results found for &nbsp;
-                            <strong>&quot;{filterName}&quot;</strong>.
-                            <br /> Try checking for typos or using complete words.
-                          </Typography>
-                        </Paper>
-                      </TableCell>
-                    </TableRow>
-                  </TableBody>
-                )}
-              </Table>
-            </TableContainer>
-          </Scrollbar>
-
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={users.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Card>
-      </Container>
-
-      <Popover
-        open={Boolean(open)}
-        anchorEl={open}
-        onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        PaperProps={{
-          sx: {
-            p: 1,
-            width: 140,
-            '& .MuiMenuItem-root': {
-              px: 1,
-              typography: 'body2',
-              borderRadius: 0.75,
-            },
-          },
-        }}
-      >
-        <MenuItem>
-          <Iconify icon={'eva:edit-fill'} sx={{ mr: 2 }} />
-          Editar
-        </MenuItem>
-
-        <MenuItem sx={{ color: 'error.main' }}>
-          <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
-          Deletar
-        </MenuItem>
-      </Popover>
-    </>
-  );
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={users.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Card>
+            </Container>
+        </>
+    );
 }
